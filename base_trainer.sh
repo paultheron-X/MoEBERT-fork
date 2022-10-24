c3f0982fea0c9effd6b778bf8161902cae715b12
Paul Theron <72143205+PaulRaid@users.noreply.github.com>
2022-10-24 10:27:55 -0400

    final script

--- base_trainer.sh
+++ base_trainer.sh
@@ -1,4 +1,4 @@
-export num_gpus=3
+export num_gpus=1
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="/home/paultheron/MoEBERT-fork/results/"
@@ -8,20 +8,21 @@ python -m torch.distributed.launch --nproc_per_node=$num_gpus \
 examples/text-classification/run_glue.py \
 --model_name_or_path bert-base-uncased \
 --task_name mnli \
---preprocess_importance \
+--per_device_train_batch_size 8 \
+--weight_decay 0.0 \
+--learning_rate 1e-5 \
+--do_train \
 --do_eval \
 --max_seq_length 128 \
---per_device_train_batch_size 24 \
---learning_rate 2e-5 \
 --num_train_epochs 7 \
---output_dir $output_dir/model_bert_full_test \
+--output_dir $output_dir/model \
 --overwrite_output_dir \
 --logging_steps 20 \
---logging_dir $output_dir/log_test \
+--logging_dir $output_dir/log \
 --report_to tensorboard \
 --evaluation_strategy steps \
 --eval_steps 2000 \
---save_strategy no \
+--save_strategy epochs \
 --warmup_ratio 0.0 \
 --seed 0 \
 --weight_decay 0.0 \
