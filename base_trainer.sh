cb45ba7b255d8569d2dfb4fb3f7cef12b71fbb57
Paul Theron <72143205+PaulRaid@users.noreply.github.com>
2022-10-24 11:08:10 -0400

    add command line args

--- base_trainer.sh
+++ base_trainer.sh
@@ -2,15 +2,21 @@ export num_gpus=1
 export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
 export PYTHONHASHSEED=0
 export output_dir="/home/paultheron/MoEBERT-fork/results/"
-export saving_dir=$output_dir/experiment_1  # Must correspond to the line in the excel hyperparameter tuning file
+echo "Script name is: $0"
+echo "Task name is $1"
+echo "experiment name is $2"
+echo "Batch size is $3"
+echo "weight decay is $4"
+echo "learning rate is $5"
+export saving_dir=$output_dir/"experiment_$2" # Must correspond to the line in the excel hyperparameter tuning file
 
 #python -m torch.distributed.launch --nproc_per_node=$num_gpus \
 python examples/text-classification/run_glue.py \
 --model_name_or_path bert-base-uncased \
---task_name mnli \
---per_device_train_batch_size 8 \
---weight_decay 0.0 \
---learning_rate 1e-5 \
+--task_name $1 \
+--per_device_train_batch_size $3 \
+--weight_decay $4 \
+--learning_rate $5 \
 --do_train \
 --do_eval \
 --max_seq_length 128 \
