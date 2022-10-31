10e10f9d3e83a395c723682822e5d7b12c7fc0e1
Paul Theron <72143205+PaulRaid@users.noreply.github.com>
2022-10-31 11:02:01 -0400

    eval strategies

--- sh_scripts/base_trainer.sh
+++ sh_scripts/base_trainer.sh
@@ -12,27 +12,54 @@ echo "eval steps is $6"
 export saving_dir=$output_dir/"experiment_$2" # Must correspond to the line in the excel hyperparameter tuning file
 
 #python -m torch.distributed.launch --nproc_per_node=$num_gpus \
-python examples/text-classification/run_glue.py \
---model_name_or_path bert-base-uncased \
---task_name $1 \
---per_device_train_batch_size $3 \
---weight_decay $4 \
---learning_rate $5 \
---do_train \
---do_eval \
---do_predict \
---max_seq_length 128 \
---num_train_epochs 10 \
---output_dir $saving_dir/model \
---overwrite_output_dir \
---logging_steps 20 \
---logging_dir $saving_dir/log \
---report_to tensorboard \
---evaluation_strategy steps \
---eval_steps 1000 \
---save_strategy epoch \
---load_best_model_at_end True \
---warmup_ratio 0.0 \
---seed 0 \
---weight_decay 0.0 \
---fp16 \
+if [ $1 = 'cola' ] || [ $1 = 'rte' ] || [ $1 = 'mrpc' ]
+then
+    python examples/text-classification/run_glue.py \
+    --model_name_or_path bert-base-uncased \
+    --task_name $1 \
+    --per_device_train_batch_size $3 \
+    --weight_decay $4 \
+    --learning_rate $5 \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    --max_seq_length 128 \
+    --num_train_epochs 10 \
+    --output_dir $saving_dir/model \
+    --overwrite_output_dir \
+    --logging_steps 20 \
+    --logging_dir $saving_dir/log \
+    --report_to tensorboard \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --warmup_ratio 0.0 \
+    --seed 0 \
+    --weight_decay 0.0 \
+    --fp16 \
+else
+    python examples/text-classification/run_glue.py \
+    --model_name_or_path bert-base-uncased \
+    --task_name $1 \
+    --per_device_train_batch_size $3 \
+    --weight_decay $4 \
+    --learning_rate $5 \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    --max_seq_length 128 \
+    --num_train_epochs 10 \
+    --output_dir $saving_dir/model \
+    --overwrite_output_dir \
+    --logging_steps 20 \
+    --logging_dir $saving_dir/log \
+    --report_to tensorboard \
+    --evaluation_strategy steps \
+    --eval_steps 1000 \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --warmup_ratio 0.0 \
+    --seed 0 \
+    --weight_decay 0.0 \
+    --fp16 \
+fi
