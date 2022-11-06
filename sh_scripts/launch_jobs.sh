1525063e6cfad20beb0b0c21982d6d1f949f454e
Paul Theron Gcloud <paulth@mit.edu>
2022-11-06 20:31:37 +0000

    script

--- sh_scripts/launch_jobs.sh
+++ sh_scripts/launch_jobs.sh
@@ -3,10 +3,18 @@ echo "Launching experiment $2"
 
 if [ $1 = 'cola' ]
 then
-    bash sh_scripts/hyper_param_distil_metatuner.sh cola $2 3e-5 32 0.01 7
+    bash sh_scripts/hyper_param_distil_metatuner.sh cola 1 3e-5 32 0.01 7
+    bash sh_scripts/hyper_param_distil_metatuner.sh cola 2 3e-5 32 0.01 7
+    bash sh_scripts/hyper_param_distil_metatuner.sh cola 3 3e-5 32 0.01 7
+    bash sh_scripts/hyper_param_distil_metatuner.sh cola 4 3e-5 32 0.01 7
+    bash sh_scripts/hyper_param_distil_metatuner.sh cola 5 3e-5 32 0.01 7
 elif [ $1 = 'rte' ]
 then
-    bash sh_scripts/hyper_param_distil_metatuner.sh rte $2 3e-5 16 0 2
+    bash sh_scripts/hyper_param_distil_metatuner.sh rte 1 3e-5 16 0 2
+    bash sh_scripts/hyper_param_distil_metatuner.sh rte 2 3e-5 16 0 2
+    bash sh_scripts/hyper_param_distil_metatuner.sh rte 3 3e-5 16 0 2
+    bash sh_scripts/hyper_param_distil_metatuner.sh rte 4 3e-5 16 0 2
+    bash sh_scripts/hyper_param_distil_metatuner.sh rte 5 3e-5 16 0 2
 elif [ $1 = 'mrpc' ]
 then
     bash sh_scripts/hyper_param_distil_metatuner.sh mrpc $2 4e-5 8 0 2
