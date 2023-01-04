72bff22b3b8c3a76bbdc7b6db4f48bb424c992ad
Paul Theron <72143205+PaulRaid@users.noreply.github.com>
2023-01-04 11:15:10 +0100

    .

--- sh_scripts/fetching/sh_dl_ds.sh
+++ sh_scripts/fetching/sh_dl_ds.sh
@@ -5,5 +5,5 @@ mkdir -p $HF_LOCAL_DIR
 HF_USER_DIR="/home/gridsan/$(whoami)/.cache/huggingface"
 export HF_HOME="${HF_LOCAL_DIR}"
 
-python sh_scripts/fetching/sh_dl_ds.py
+python sh_scripts/fetching/dl_ds.py
 rsync -a --ignore-existing ${HF_LOCAL_DIR}/ $HF_USER_DIR
