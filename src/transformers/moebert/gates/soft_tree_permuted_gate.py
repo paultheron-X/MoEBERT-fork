35817785394abce4a43fdbaa96c98cb4111612af
Paul Theron Supercloud <paulth@mit.edu>
2023-01-12 17:04:27 -0500

    rm prints

--- src/transformers/moebert/gates/soft_tree_permuted_gate.py
+++ src/transformers/moebert/gates/soft_tree_permuted_gate.py
@@ -212,8 +212,8 @@ class SoftTreePermutedGate(nn.Module):
                 w_concat = torch.unsqueeze(w_concat, dim=1)  # (b, 1, k, nb_experts)
 
                 # w_concat: (b, 1, k, nb_experts), perm_mask: [k, nb_experts, nb_experts]
-                print("w_concat shape: ", w_concat.shape)
-                print("perm_mask shape: ", permutation_weights.shape)
+                #print("w_concat shape: ", w_concat.shape)
+                #print("perm_mask shape: ", permutation_weights.shape)
                 
                 #permutation_weights = torch.unsqueeze(permutation_weights, dim=0)  # (1, k, nb_experts, nb_experts)
                 w_permuted = torch.einsum("bijk,jkl->bijl", w_concat, permutation_weights)
