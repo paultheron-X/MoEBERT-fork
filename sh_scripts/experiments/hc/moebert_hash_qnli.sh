40bc5a38785eea250b82b78e688e32748efb6e06
Paul Theron Supercloud <paulth@mit.edu>
2023-01-27 16:13:18 -0500

    scripts to launch qqp and qnli w/ optimal params from the paper

---
+++ sh_scripts/experiments/hc/moebert_hash_qnli.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+export num_gpus=3
+export CUBLAS_WORKSPACE_CONFIG=":16:8" # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+export PYTHONHASHSEED=0
+echo "Script name is: $0"
+
+
+
+export LOCAL_OUTPUT="/home/gridsan/$(whoami)/MoEBERT-fork/results"
+
+export output_dir="$LOCAL_OUTPUT/qnli"
+export saving_dir=$output_dir/"qnli_hash_experiment" # Must correspond to the line in the excel hyperparameter tuning file
+export original_model_dir=$output_dir/"experiment_qnli_finetuned_model"
+
+export metric_for_best_model="accuracy"
+export num_epochs=15
+
+
+echo "Number of epochs is $num_epochs"
+
+python examples/text-classification/run_glue.py \
+    --model_name_or_path $original_model_dir/model \
+    --task_name qnli \
+    --per_device_train_batch_size 16 \
+    --weight_decay 0.0 \
+    --learning_rate 2e-5 \
+    --do_train \
+    --do_eval \
+    --do_predict \
+    --max_seq_length 128 \
+    --num_train_epochs $num_epochs \
+    --output_dir $saving_dir/model \
+    --overwrite_output_dir \
+    --logging_steps 20 \
+    --logging_dir $saving_dir/log \
+    --report_to tensorboard \
+    --evaluation_strategy steps \
+    --eval_steps 1000 \
+    --save_strategy epoch \
+    --load_best_model_at_end False \
+    --metric_for_best_model $metric_for_best_model \
+    --warmup_ratio 0.0 \
+    --seed 0 \
+    --ignore_data_skip True \
+    --fp16 \
+    --moebert moe \
+    --moebert_distill 2.0 \
+    --moebert_expert_num 4 \
+    --moebert_expert_dim 768 \
+    --moebert_expert_dropout 0.1 \
+    --moebert_load_balance 0.0 \
+    --moebert_load_importance $original_model_dir/importance_qnli.pkl \
+    --moebert_route_method hash-random \
+    --moebert_share_importance 512 \
+    --moebert_gate_entropy 1 \
+    --moebert_gate_gamma 1
---
+++ sh_scripts/supercloud/hardcoded/submit_hash_opt_qnli.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH --job-name hash_qnli_opt
+#SBATCH --gres=gpu:volta:2
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=21-00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=paulth@mit.edu
+#SBATCH --output=/home/gridsan/ptheron/MoEBERT-fork/logs/hash_qnli_out%j.txt
+#SBATCH --error=/home/gridsan/ptheron/MoEBERT-fork/logs/hash_qnli_err%j.txt
+
+# Initialize the module command first
+source /etc/profile
+
+# Load modules
+module load anaconda/2021b
+#module load gurobi/gurobi-903
+
+# Call your script as you would from your command line
+source activate MoEBERT
+
+export TOTAL_GPUS=${SLURM_NTASKS}
+export GPUS_PER_NODE=2
+
+echo "Total number of GPUs: $TOTAL_GPUS"
+echo "GPUs per node: $GPUS_PER_NODE"
+
+if [ ! -e /proc/$(pidof nvidia-smi) ]
+then
+	echo "nvidia-smi does not seem to be running. exiting job"
+    exit 1
+fi
+
+HF_USER_DIR="/home/gridsan/$(whoami)/.cache/huggingface"
+HF_LOCAL_DIR="/state/partition1/user/$(whoami)/cache/huggingface"
+mkdir -p $HF_LOCAL_DIR
+rsync -a --ignore-existing $HF_USER_DIR/ ${HF_LOCAL_DIR}
+export HF_HOME=${HF_LOCAL_DIR}
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export WANDB_DISABLED="true"
+
+export BACKEND="pytorch"
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+cd /home/gridsan/$(whoami)/MoEBERT-fork
+
+export output_dir="/home/gridsan/$(whoami)/MoEBERT-fork/results"
+
+bash sh_scripts/experiments/hc/moebert_hash_qnli.sh
